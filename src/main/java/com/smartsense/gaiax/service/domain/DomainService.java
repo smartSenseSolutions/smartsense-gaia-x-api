8b21d69ede59d52032dd30e8a587f6b7c187924d
Nitin Vavdiya <nitin.vavdiya@smartsensesolutions.com>
2023-04-20 18:46:38 +0530

    ssl job retry removed

--- src/main/java/com/smartsense/gaiax/service/domain/DomainService.java
+++ src/main/java/com/smartsense/gaiax/service/domain/DomainService.java
@@ -172,7 +172,7 @@ public class DomainService {
 
     private void createCertificateCreationJob(long enterpriseId, Enterprise enterprise) {
         try {
-            scheduleService.createJob(enterpriseId, StringPool.JOB_TYPE_CREATE_CERTIFICATE, 3); //try for 3 time for certificate
+            scheduleService.createJob(enterpriseId, StringPool.JOB_TYPE_CREATE_CERTIFICATE, 0); //try for 3 time for certificate
         } catch (SchedulerException e) {
             LOGGER.error("Can not create certificate creation job for enterprise->{}", enterprise, e);
             enterprise.setStatus(RegistrationStatus.CERTIFICATE_CREATION_FAILED.getStatus());
